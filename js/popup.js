34263f3d26c6307bb27db7e2476b814bcbc1d09e
Mario Basic <mario.basic@outlook.com>
2014-12-03 23:01:41 +0100

    Learning about desktop notifications.

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,30 @@
-/*
-chrome.notifications.create(1, {
+chrome.notifications.create('example-notification', {
     type: "basic",
-    title: "Primary Title",
-    message: "Primary message to display"
-});*/
+    title: "Case Study: The Laravel Installer",
+    message: "To continue our learning, let's review the makeup of Laravel's command-line installer tool. In fact, we'll reproduce it from scratch!",
+    iconUrl: "laracasts-logo.png",
+    buttons: [
+        {
+            title: 'Watch'
+        },
+        {
+            title: 'Mark as watched'
+        }
+
+    ]
+}, function() {});
+
+
+chrome.notifications.onButtonClicked.addListener(function(notificationId, buttonIndex)
+{
+    if(buttonIndex == 0)
+    {
+        alert('Watch lesson');
+    }
+    if(buttonIndex == 1)
+    {
+        alert('Mark lesson as watched')
+    }
+
+
+});
